8275d8532ea8960215d153a265ebffa940edd30a
uttampatil07dipa <uttampatil07@gmail.com>
2026-05-15 10:48:56 +0530

    Change spreadsheet ID for Google Sheet access
    Updated the spreadsheet ID in update_sheet.py.

--- update_sheet.py
+++ update_sheet.py
@@ -16,7 +16,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # अपनी गूगल शीट की ID यहाँ डालें (URL के बीच का हिस्सा)
-spreadsheet_id = "यहाँ_अपनी_शीट_की_ID_डालें" 
+spreadsheet_id = "1gV3p114fXS4bkMgjKbLnq_Lk6cd3ekvv4-Nv6QPW0YI" 
 worksheet = client.open_by_key(spreadsheet_id).worksheet("Top 250 Stocks")
 
 # 2. NSE UDiFF Data Fetcher
